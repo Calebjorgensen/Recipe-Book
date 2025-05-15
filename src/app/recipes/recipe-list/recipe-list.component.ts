7a522716e27e42d05b79c7332c24b0f011b10d29
Calebjorgensen <121927129+Calebjorgensen@users.noreply.github.com>
2025-05-14 21:02:20 -0700

    Initial Commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  standalone: false,
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test recipe', 'This is simply a test',
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Ffast-food&psig=AOvVaw3HUSIDtg2kjPhRqbr3mi6h&ust=1747188558263000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj97eWun40DFQAAAAAdAAAAABAE')
+  ];
+
+  constructor() { }
+
+}
