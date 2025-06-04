15ca004f11dbc0dc8b028decc27a3abefd6c24c0
Calebjorgensen <121927129+Calebjorgensen@users.noreply.github.com>
2025-06-04 11:41:28 -0700

    added the services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,8 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +14,27 @@ export class RecipeService {
 
    private recipes: Recipe[] = [
       new Recipe('A Test recipe', 'This is simply a test',
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Ffast-food&psig=AOvVaw3HUSIDtg2kjPhRqbr3mi6h&ust=1747188558263000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj97eWun40DFQAAAAAdAAAAABAE'),
-      new Recipe('Recipe 2', 'Here is how to make...', '')
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Ffast-food&psig=AOvVaw3HUSIDtg2kjPhRqbr3mi6h&ust=1747188558263000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj97eWun40DFQAAAAAdAAAAABAE',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+      new Recipe('Recipe 2', 'Here is how to make...', '', 
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
     ];
 
+    constructor(private slService: ShoppingListService) {}
 
     getRecipes() {
       return this.recipes.slice();
     }
 
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+      this.slService.addIngredients(ingredients);
+    }
 
-  constructor() { }
+  
 }
