13316535a2d23273b13c7fa2731b0e4bf30fa021
Calebjorgensen <121927129+Calebjorgensen@users.noreply.github.com>
2025-06-14 17:55:32 -0700

    Child route add

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,14 +9,10 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent {
 
-  
   @Input() recipe: Recipe;
+  @Input() index: number;
+
   
-  constructor(private recipeService: RecipeService){}
-  
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
 
 
 }
