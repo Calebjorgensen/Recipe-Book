2dc80d3a2368e49039af579ef7e4f0d90afbbfc0
Calebjorgensen <121927129+Calebjorgensen@users.noreply.github.com>
2025-06-19 12:05:16 -0700

    udpated Observales

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,13 +4,13 @@ import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
 
-  recipeSelected = new EventEmitter<Recipe>();
 
    private recipes: Recipe[] = [
       new Recipe('A Test recipe', 'This is simply a test',
